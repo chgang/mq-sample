4713c55fa55ed91fe6fd914b781ced1a1e20b313
chgang <chenggangsy@163.com>
2018-07-28 12:43:56 +0800

    client handler

--- src/main/java/com/qskx/mq/rpc/client/NettyClientHandler.java
+++ src/main/java/com/qskx/mq/rpc/client/NettyClientHandler.java
@@ -1,5 +1,6 @@
 package com.qskx.mq.rpc.client;
 
+import com.qskx.mq.rpc.client.model.RpcCallBackFuture;
 import com.qskx.mq.rpc.client.model.RpcResponse;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -15,6 +16,14 @@ public class NettyClientHandler extends SimpleChannelInboundHandler<RpcResponse>
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, RpcResponse rpcResponse) throws Exception {
+        RpcCallBackFuture future = RpcCallBackFuture.futurePool.get(rpcResponse.getRequestId());
+        future.setResponse(rpcResponse);
+        RpcCallBackFuture.futurePool.put(rpcResponse.getRequestId(), future);
+    }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error(">>>>>>>>>>> netty client caught exception", cause);
+        ctx.close();
     }
 }
